42feccc593b2a8c59ef02da3b22c3c3610a89d17
Samuel Machado <sam.machado16@gmail.com>
2026-03-12 13:03:33 -0300

    Tipos primitivos de variáveis

---
+++ ex004/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testes tipos primitivos</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        $num = 300;
+        echo "O valor da variável é $num ";
+        var_dump($num);
+        // 3e2 = 3 x (10 ^ 2)
+
+        $valor = (int) "900";
+        var_dump($valor)
+        
+        ?>
+</body>
+</html>
