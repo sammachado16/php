9e9d17c0d13fa227067edd788897fbb885c8da60
Samuel Machado <sam.machado16@gmail.com>
2026-03-12 12:36:54 -0300

    Create index.php

---
+++ ex003/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variáveis</title>
+</head>
+<body>
+    <h1>Testando variáveis em PHP</h1>
+    <?php 
+    $nome = "Samuel";
+    $sobrenome = "Machado";
+
+    echo "Muito prazer, $nome $sobrenome!"
+    ?>
+</body>
+</html>
