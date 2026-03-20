e7bfbce31fe1c43e866141543caeaf290ffef7b4
Samuel Machado <sam.machado16@gmail.com>
2026-03-20 20:40:05 -0300

    Final do formulário retroalimentado

--- ex008/index.php
+++ ex008/index.php
@@ -14,11 +14,19 @@
     <main>
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="get">
             <label for="v1">Valor 1</label>
-            <input type="number" name="v1" id="v1">
+            <input type="number" name="v1" id="v1" value="<?=$valor1?>">
             <label for="v2">Valor 2</label>
-            <input type="number" name="v2" id="v2">
+            <input type="number" name="v2" id="v2" value="<?=$valor2?>">
             <input type="submit" value="Somar">
         </form>
     </main>
+
+    <section id="resultado">
+        <h2>Resultado da soma</h2>
+        <?php 
+            $soma = $valor1 + $valor2;
+            echo "<p>A soma dos valores é <strong>$soma</strong>"
+        ?>
+    </section>
 </body>
 </html>
