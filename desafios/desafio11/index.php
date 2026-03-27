b36325260cb16eec13793a9dce23d7d2dad62f3d
Samuel Machado <sam.machado16@gmail.com>
2026-03-26 23:00:49 -0300

    Arrumados problemas

--- desafios/desafio11/index.php
+++ desafios/desafio11/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio 11</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?v=<?php echo time(); ?>">
 </head>
 <body>
     <?php 
@@ -17,8 +17,8 @@
             <label for="preco">Preço do Produto (R$)</label>
             <input type="number" id="preco" name="preco" value="<?=$preco?>">
 
-            <label for="reajuste">Qual será o percentual de reajuste? (<strong><?=$reajuste?>%</strong>)</label>
-            <input type="range" id="reajuste" name="reajuste" value="<?=$reajuste?>">
+            <label for="reajuste">Qual será o percentual de reajuste? (<strong><span id="reaj">?</span>%</strong>)</label>
+            <input type="range" id="reajuste" name="reajuste" value="<?=$reajuste?>" oninput="mudaValor()">
 
             <input type="submit" value="Reajustar">
         </form>
@@ -32,6 +32,15 @@
         echo "O produto que custava " . numfmt_format_currency($conversor, $preco, "BRL") . " com <strong>$reajuste% de aumento</strong> vai passar a custar <strong>" . numfmt_format_currency($conversor, $valor_rj, "BRL") . "</strong> a partir de agora.";
         ?>
     </section>
+    <script>
+        //Declarações automáticas
+        mudaValor()
 
+        function mudaValor() {
+            let texto = document.querySelector("span#reaj");
+            let valor = document.querySelector("input#reajuste")
+            texto.innerHTML = valor.value
+        }
+    </script>
 </body>
 </html>
