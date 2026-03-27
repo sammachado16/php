6c3ab03be560c4e74e6680700c8e949ea65fac2f
Samuel Machado <sam.machado16@gmail.com>
2026-03-26 22:43:31 -0300

    Update index.php

--- desafios/desafio10/index.php
+++ desafios/desafio10/index.php
@@ -8,17 +8,18 @@
 </head>
 <body>
     <?php 
-        $ano_nasc = $_GET['ano_nasc'] ?? 0;
-        $ano_atual = $_GET['ano_atual'] ?? (int)date("Y");
+        $atual = intval(date("Y"));
+        $ano_nasc = $_GET['ano_nasc'] ?? "2026";
+        $ano_escolhido = $_GET['ano_escolhido'] ?? $atual;
     ?>
     <main>
         <h1>Médias Aritméticas</h1>
         <form action="<?= $_SERVER['PHP_SELF']?>" method="get">
             <label for="ano_nasc">Em que ano você nasceu?</label>
-            <input type="number" id="ano_nasc" name="ano_nasc" value="<?=$ano_nasc?>">
+            <input type="number" id="ano_nasc" name="ano_nasc" min="1900" max="<?=($atual-1)?>" value="<?=$ano_nasc?>">
 
-            <label for="ano_atual">Quer saber sua idade em que ano? (atualmente estamos em <strong>2026</strong>)</label>
-            <input type="number" id="ano_atual" name="ano_atual" value="<?=$ano_atual?>">
+            <label for="ano_escolhido">Quer saber sua idade em que ano? (atualmente estamos em <strong><?=$atual?></strong>)</label>
+            <input type="number" id="ano_escolhido" name="ano_escolhido" value="<?=$ano_escolhido?>">
 
             <input type="submit" value="Qual será a minha idade?">
         </form>
@@ -26,8 +27,8 @@
     <section>
         <h1>Resultado</h1>
         <?php
-        $idade = $ano_atual - $ano_nasc;
-          echo "Quem nasceu em $ano_nasc vai ter <strong>$idade anos</strong> em $ano_atual";
+        $idade = $ano_escolhido - $ano_nasc;
+          echo "Quem nasceu em $ano_nasc vai ter <strong>$idade anos</strong> em $ano_escolhido";
         ?>
     </section>
 
