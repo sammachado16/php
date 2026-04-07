37c8cd67976c09da4a9750f00c3f07510eb31074
Samuel Machado <sam.machado16@gmail.com>
2026-04-06 22:47:53 -0300

    PHP POO

---
+++ poo/Aula02/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Aula 02 POO</title>
+    </head>
+    <body>
+        <?php
+            require_once 'Caneta.php';
+            $c1 = new Caneta;
+            $c1->cor = "Azul";
+            $c1->ponta = 0.5;
+            $c1->tampada = true;
+            $c1->destampar();
+            $c1->rabiscar();
+            print_r($c1);
+            
+            echo "<br>";
+            
+            $c2 = new Caneta;
+            $c2->cor = "Vermelha";
+            $c2->carga = 50;
+            $c2->tampar();
+            //var_dump($c1);
+            print_r($c2);
+        ?>
+    </body>
+</html>
