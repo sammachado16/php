24a3a18c53203deadaaf1960bef07d5f09c308cb
Samuel Machado <sam.machado16@gmail.com>
2026-04-08 12:59:37 -0300

    Aula 4 prática

---
+++ poo/Aula04/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <pre>
+        <?php
+            require_once 'Caneta.php';
+            $c1 = new Caneta("BIC", "Azul", 0.5);
+            print_r($c1); 
+        ?>
+        </pre>
+    </body>
+</html>
